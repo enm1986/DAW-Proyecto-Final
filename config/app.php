ec515a505fa90f4754a98aca72822bd388ead291
eugenio <eugenio@debian-eugenio>
2020-07-06 16:47:34 +0200

    Cambiado Nombre de la aplicación

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Proyecto'),
+    'name' => env('APP_NAME', 'Administrador de Comunidades'),
 
     /*
     |--------------------------------------------------------------------------
