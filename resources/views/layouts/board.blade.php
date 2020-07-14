f35106f78d120637e4fc28adc030feb8a3283d57
eugenio <eugenio@debian-eugenio>
2020-07-15 00:05:59 +0200

    Gastos, cuotas e ingresos Informes usuario

--- resources/views/layouts/board.blade.php
+++ resources/views/layouts/board.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-    <nav class="navbar navbar-dark bg-secondary mb-4 p-2 shadow">
+    <nav class="navbar navbar-dark bg-secondary mb-4 p-2 shadow justify-content-center justify-content-sm-between">
         <div class="navbar-nav flex-row">
             <a class="btn btn-light m-1 " href="/home">Mis comunidades</a>
             <a class="btn btn-light m-1" href="/comunidad/{{ $comunidad->id }}">Portada</a>
         </div>
-        <div class="d-flex flex-wrap align-items-center">
+        <div class="d-flex flex-wrap-reverse align-items-center flex-sm-wrap">
             <span class="text-light text-uppercase font-weight-bold m-2">{{ $comunidad->nombre }} </span>
             @if($acceso == 'admin')
             <a class="btn btn-outline-light m-1" href="/comunidad/{{ $comunidad->id }}/admin" role="button">Administrar</a>
